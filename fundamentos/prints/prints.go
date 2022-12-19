0218185d6160dd0a865262b8352ca65b81f6fa2c
jose-cleiton <cleitons835@gmail.com>
2022-12-19 13:28:02 -0300

    feat: :sparkles: imprimir variáveis como no c

--- fundamentos/prints/prints.go
+++ fundamentos/prints/prints.go
@@ -15,6 +15,14 @@ func main() {
 	xs := fmt.Sprint(x)
 	fmt.Println("O valor de x é " + xs)
 	fmt.Println("O valor de x é", x)
+	fmt.Printf("O valor de x é %.2f", x)
+
+// imprimir variáveis como no c
+	a := 1
+	b := 1.9999
+	c := false
+	d := "opa!"
+	fmt.Printf("%d %f %.1f %t %s", a, b, b, c, d)
 }
 
 
