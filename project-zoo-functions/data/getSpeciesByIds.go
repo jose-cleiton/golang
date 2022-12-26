1a4ab8fec7449ec5a53991e80e447806f6294664
jose-cleiton <cleitons835@gmail.com>
2022-12-26 09:36:00 -0300

    feat: :sparkles: Implemente a função getSpeciesByIds

--- project-zoo-functions/data/getSpeciesByIds.go
+++ project-zoo-functions/data/getSpeciesByIds.go
@@ -7,71 +7,60 @@ import (
 )
 
 type Species struct {
-  Id   int    `json:"id"`
-  Name string `json:"name"`
+  Id          string   `json:"id"`
+  Name        string   `json:"name"`
+  Popularity  int      `json:"popularity"`
+  Location    string   `json:"location"`
+  Availability []string `json:"availability"`
+  Residents   []struct {
+    Name string `json:"name"`
+    Sex  string `json:"sex"`
+    Age  int    `json:"age"`
+  } `json:"residents"`
 }
-type SpeciesArray []Species
 
-func getSpeciesByIds(ids ...int) (SpeciesArray, error) {
-  // ler o arquivo JSON e armazenar os dados em um array de espécies
+type ZooData struct {
+  Species []Species `json:"species"`
+}
+
+func getSpeciesByIds(ids ...string) ([]Species, error) {
+  // ler o arquivo JSON e armazenar os dados em uma variável do tipo ZooData
   data, err := ioutil.ReadFile("data/zoo_data.json")
   if err != nil {
     return nil, err
   }
-  var speciesArray SpeciesArray
-  json.Unmarshal(data, &speciesArray)
-
-  // verificar se o array de ids é vazio
-  if len(ids) == 0 {
-    return []Species{}, nil
-  }
+  var dataMap ZooData
+  json.Unmarshal(data, &dataMap)
 
   // criar o array de retorno da função
-  var result SpeciesArray
+  var result []Species
 
   // adicionar as espécies correspondentes aos ids passados como parâmetro
   for _, id := range ids {
     found := false
-    for _, species := range speciesArray {
+    for _, species := range dataMap.Species {
       if species.Id == id {
-        result = append(result, species)
         found = true
+        result = append(result, species)
         break
       }
     }
     if !found {
-      return nil, fmt.Errorf("species with id %d not found", id)
+      return nil, fmt.Errorf("species with id %s not found", id)
     }
   }
-
   return result, nil
 }
 
 func main() {
-  // chamar a função getSpeciesByIds com diferentes conjuntos de ids
-  species, err := getSpeciesByIds()
+  id1 := "0938aa23-f153-4937-9f88-4858b24d6bce"
+  id2 := "ef3778eb-2844-4c7c-b66c-f432073e1c6b"
+
+  // chamar a função getSpeciesByIds passando os ids como parâmetros
+  species, err := getSpeciesByIds(id1, id2)
   if err != nil {
     fmt.Println(err)
   } else {
-    fmt.Println("species with no id:", species)
-  }
-
-  species, err = getSpeciesByIds(1)
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    fmt.Println("species with id 1:", species)
-  }
-
-  species, err = getSpeciesByIds(1, 2, 3)
-  if err != nil {
-    fmt.Println(err)
-  } else {
-    fmt.Println("species with ids 1, 2, and 3:", species)
-  }
-
-  species, err = getSpeciesByIds(4, 5, 6)
-  if err != nil {
-    fmt.Println(err)
+    fmt.Println("espécies com id", id1, "e", id2, ":", species)
   }
 }
