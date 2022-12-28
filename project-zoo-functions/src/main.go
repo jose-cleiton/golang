150c06e3061c67a9e557d6e23f69bf05460c9f8c
jose-cleiton <cleitons835@gmail.com>
2022-12-28 17:16:39 -0300

    fix: :sparkles: corrigindo main

--- project-zoo-functions/src/main.go
+++ project-zoo-functions/src/main.go
@@ -13,37 +13,50 @@ import (
 
 func main() {
 	zoo:=data.Data
+
+// 	isManager := data.IsManager("burlId", zoo)
 	
-	allLionsOlderThan12 := data.GetAnimalsOlderThan(zoo, "lions", 12)
+// 	allLionsOlderThan12 := data.GetAnimalsOlderThan(zoo, "lions", 12)
 
-	allTigersOlderThan5 := data.GetAnimalsOlderThan(zoo, "tigers", 5)
+// 	allTigersOlderThan5 := data.GetAnimalsOlderThan(zoo, "tigers", 5)
 
-	speciesById := data.GetSpeciesByIds(zoo, "0938aa23-f153-4937-9f88-4858b24d6bce")
+// 	speciesById := data.GetSpeciesByIds(zoo, "0938aa23-f153-4937-9f88-4858b24d6bce")
 
-	employee := data.GetEmployeeByName(zoo.Employees, "Sharonda", "Spry")
+// 	employee := data.GetEmployeeByName(zoo.Employees, "Sharonda", "Spry")
 
-	fmt.Println("-->Resultado da fucção: GetAnimalsOlderThan<--")
-	fmt.Println(allLionsOlderThan12)
-	fmt.Println(allTigersOlderThan5)
+// 	fmt.Println("-->Resultado da fucção: GetAnimalsOlderThan<--")
+// 	fmt.Println(allLionsOlderThan12)
+// 	fmt.Println(allTigersOlderThan5)
 
-	fmt.Println("-->Resultado da fucção: GetSpeciesByIds<--")
+// 	fmt.Println("-->Resultado da fucção: GetSpeciesByIds<--")
 
 	
-	fmt.Println("Nome: ", speciesById[0].Name)
-	fmt.Println("Localização: ", speciesById[0].Location)
-	fmt.Println("Disponibilidade: ", speciesById[0].Availability)
-	fmt.Println("Residentes: ", speciesById[0].Residents)
+// 	fmt.Println("Nome: ", speciesById[0].Name)
+// 	fmt.Println("Localização: ", speciesById[0].Location)
+// 	fmt.Println("Disponibilidade: ", speciesById[0].Availability)
+// 	fmt.Println("Residentes: ", speciesById[0].Residents)
 
-	fmt.Println("-->Resultado da fucção: GetEmployeeByName<--")
+// 	fmt.Println("-->Resultado da fucção: GetEmployeeByName<--")
 
   
   
-	fmt.Printf("Nome: %s %s\n", employee.FirstName, employee.LastName)
-	fmt.Printf("Responsável por: %s\n", employee.ResponsibleFor)
-	fmt.Printf("Gerente: %s\n", employee.Managers)
+// 	fmt.Printf("Nome: %s %s\n", employee.FirstName, employee.LastName)
+// 	fmt.Printf("Responsável por: %s\n", employee.ResponsibleFor)
+// 	fmt.Printf("Gerente: %s\n", employee.Managers)
 
+// fmt.Println("-->Resultado da fucção: IsManager<--")
 
-	
+// 	fmt.Println(isManager)
+
+	ger, err:= data.GetRelatedEmployees("stephanieId", zoo)
+
+	if err == nil {
+		fmt.Println(ger)
+	}else {
+
+		fmt.Println(err)
+	}
+
 }
 
 
