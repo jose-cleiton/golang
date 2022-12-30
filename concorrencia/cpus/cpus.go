f730b3c8e7d3d9e991a7488cc5cdbf84932c45da
jose-cleiton <cleitons835@gmail.com>
2022-12-30 18:03:11 -0300

    feat: :sparkles: SearchInts

--- concorrencia/cpus/cpus.go
+++ concorrencia/cpus/cpus.go
@@ -1,10 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"runtime"
-)
+import "fmt"
+
+type Array []int
+
+func (a Array) Map(callback func(int) int) Array {
+	if a == nil {
+		return nil
+	}
+
+	result := make(Array, len(a))
+	for i, v := range a {
+		result[i] = callback(v)
+	}
+	return result
+}
 
 func main() {
-	fmt.Println(runtime.NumCPU())
+	array := Array{1, 2, 3, 4, 5}
+	
+	
+	fmt.Println(array.Map(func(a,b int) int {
+			return a+b
+		}
+	)) // [2 4 6 8 10]
 }
---
+++ project-zoo-functions/src/filter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+    numbers := []int{1, 3, 5, 7, 9, 11}
+
+
+		fmt.Println(sort.SearchInts(numbers,7))
+
+    var firstEven []int
+		
+
+    for _, number := range numbers {
+        if number >= 7 {
+            firstEven = append(firstEven, number)
+            
+        }
+    }
+
+    fmt.Println(firstEven) // expected output: 2
+}
