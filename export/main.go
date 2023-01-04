3c25478c7bce1c132d4858167390a4c2141fdbf5
jose-cleiton <cleitons835@gmail.com>
2023-01-04 14:21:36 -0300

    textos

--- export/main.go
+++ export/main.go
@@ -7,15 +7,12 @@ import (
     "strings"
 )
 
-func main() {
-
-   
+func main() {   
     x := math.Sum(1,2,3)
     fmt.Println(x)
     a:=funcoes.Apply(2, funcoes.MultiplyByTwo)
     fmt.Println(a)
     j:= strings.Join([]string{"a", "b", "c"}, ",")
-    fmt.Println(j)
-    
-   
+    fmt.Println(j)   
 }
+
